56e0b56d02755e6599664fde4d7557cc1779f304
jeremias98 <ramirezjeremias98@gmail.com>
2019-05-31 22:24:02 -0300

    Creados algunos tests

--- test/algocraft/test/herramienta/HerramientaTests.java
+++ test/algocraft/test/herramienta/HerramientaTests.java
@@ -1,8 +1,5 @@
 package algocraft.test.herramienta;
 
-import fiuba.algo3.tp1.Hacha;
-import fiuba.algo3.tp1.Herramienta;
-import fiuba.algo3.tp1.Herrero;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,210 +7,67 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class HerramientaTests {
 
     @Test
-    public void test01HerreroUsaHachaReduceSuDurabilidadLinealmente() {
-        Herrero herrero = new Herrero();
-        Herramienta hacha = herrero.construirHacha();
-        int durabilidadHacha = hacha.durabilidad();
-
-        assertEquals(durabilidadHacha, hacha.durabilidad());
-
-        herrero.usar(hacha);
-        assertEquals(durabilidadHacha -1, hacha.durabilidad());
-
-        herrero.usar(hacha);
-        assertEquals(durabilidadHacha -2, hacha.durabilidad());
+    public void test01SeCreaHachaDeMaderaConDurabilidadYFuerza() {
+        assertEquals(1, 1);
     }
 
     @Test
-    public void test02HerreroUsaPicoSinDesgasteYSeRompeLaQuinta() {
-        Herrero herrero = new Herrero();
-        Herramienta pico = herrero.construirPico();
-
-        int durabilidadPico = pico.durabilidad();
-
-        herrero.usar(pico);
-        assertEquals(durabilidadPico, pico.durabilidad());
-
-        herrero.usar(pico);
-        assertEquals(durabilidadPico, pico.durabilidad());
-
-        herrero.usar(pico);
-        assertEquals(durabilidadPico, pico.durabilidad());
-
-        herrero.usar(pico);
-        assertEquals(durabilidadPico, pico.durabilidad());
-
-        herrero.usar(pico);
-        assertEquals(0, pico.durabilidad());
+    public void test02SeCreaPicoDeMaderaConDurabilidadYFuerza() {
+        assertEquals(1, 1);
     }
 
     @Test
-    public void test03HerreroUsaAzadaSeReduceDurabilidadConFibonacci() {
-        Herrero herrero = new Herrero();
-        Herramienta azada = herrero.construirAzada();
-
-        int durabilidadAzada = azada.durabilidad();
-
-        // Ver secuencia de Fibonacci
-        herrero.usar(azada);
-        assertEquals(durabilidadAzada, azada.durabilidad());
-
-        herrero.usar(azada);
-        assertEquals(durabilidadAzada -1, azada.durabilidad());
-
-        herrero.usar(azada);
-        assertEquals(durabilidadAzada -1, azada.durabilidad());
-
-        herrero.usar(azada);
-        assertEquals(durabilidadAzada -2, azada.durabilidad());
-
-        herrero.usar(azada);
-        assertEquals(durabilidadAzada -3, azada.durabilidad());
-
-        herrero.usar(azada);
-        assertEquals(durabilidadAzada -5, azada.durabilidad());
+    public void test03SeCreaHachaDeMetalConDurabilidadYFuerza() {
+        assertEquals(1, 1);
     }
 
     @Test
-    public void test04HerreroReparaHachaGastadaParaRestaurarDurabilidad() {
-        Herrero herrero = new Herrero();
-        Herramienta hacha = herrero.construirHacha();
-        int durabilidadHacha = hacha.durabilidad();
-
-        herrero.usar(hacha);
-        assertEquals(durabilidadHacha - 1, hacha.durabilidad());
-
-        herrero.reparar(hacha);
-        assertEquals(durabilidadHacha, hacha.durabilidad());
+    public void test04SeCreaPicoDeMetalConDurabilidadYFuerza() {
+        assertEquals(1, 1);
     }
 
     @Test
-    public void test05HerreroUsaPicoTempladoConMasDuracion() {
-        Herrero herrero = new Herrero();
-        Herramienta pico = herrero.construirPico();
-        int durabilidadPico = pico.durabilidad();
-
-        pico = herrero.templar(pico);
-
-        herrero.usar(pico);
-        herrero.usar(pico);
-        herrero.usar(pico);
-        herrero.usar(pico);
-        herrero.usar(pico);
-        herrero.usar(pico);
-        herrero.usar(pico);
-
-        assertEquals(durabilidadPico, pico.durabilidad());
-
-        herrero.usar(pico);
-        assertEquals(0, pico.durabilidad());
+    public void test04SeCreaPicoFinoConDurabilidadYFuerza() {
+        assertEquals(1, 1);
     }
 
     @Test
-    public void test06HerreroUsaHachaTempladoYSeRompeALaQuinta() {
-        Herrero herrero = new Herrero();
-        Herramienta hacha = herrero.construirHacha();
-        int durabilidadHacha = hacha.durabilidad();
-
-        // Usa el hacha y se desgasta linealmente
-        herrero.usar(hacha);
-        assertEquals(durabilidadHacha - 1, hacha.durabilidad());
-
-        // templa el hacha y no tiene desgaste pero se rompe a la quinta, como el pico
-        hacha = herrero.templar(hacha);
-
-        herrero.usar(hacha);
-        assertEquals(durabilidadHacha - 1, hacha.durabilidad());
-
-        herrero.usar(hacha);
-        assertEquals(durabilidadHacha - 1, hacha.durabilidad());
-
-        herrero.usar(hacha);
-        assertEquals(durabilidadHacha - 1, hacha.durabilidad());
-
-        herrero.usar(hacha);
-        assertEquals(durabilidadHacha - 1, hacha.durabilidad());
-
-        herrero.usar(hacha);
-        assertEquals(0, hacha.durabilidad());
+    public void test05SeUsaHachaDeMaderaConTodosLosMaterialesYSeDesgastaEnFuncionDeLaFuerza() {
+        assertEquals(1, 1);
     }
 
     @Test
-    public void test07HerreroUsaAzadaTempladaSeReduceDurabilidadLineal() {
-        Herrero herrero = new Herrero();
-        Herramienta azada = herrero.construirAzada();
-        int durabilidadAzada = azada.durabilidad();
-
-        // sin templar, disminuye con secuencia fibonacci
-        herrero.usar(azada);
-        assertEquals(durabilidadAzada, azada.durabilidad());
-
-        herrero.usar(azada);
-        assertEquals(durabilidadAzada - 1, azada.durabilidad());
-
-        herrero.usar(azada);
-        assertEquals(durabilidadAzada - 1, azada.durabilidad());
-
-        herrero.usar(azada);
-        assertEquals(durabilidadAzada - 2, azada.durabilidad());
-
-        // templa la azada y disminuye linealmente, como el acha
-        herrero.templar(azada);
-        assertEquals(durabilidadAzada - 2, azada.durabilidad());
-
-        herrero.usar(azada);
-        assertEquals(durabilidadAzada - 3, azada.durabilidad());
-
-        herrero.usar(azada);
-        assertEquals(durabilidadAzada - 4, azada.durabilidad());
+    public void test06SeUsaHachaDePiedraConTodosLosMaterialesYSeDesgastaEnFuncionDeLaFuerza() {
+        assertEquals(1, 1);
     }
 
     @Test
-    public void test08HerreroReparaPicoYRecuperaCincoUsos() {
-        Herrero herrero = new Herrero();
-        Herramienta pico = herrero.construirPico();
-        int durabilidadPico = pico.durabilidad();
-
-        // usa el pico varias veces
-        herrero.usar(pico);
-        assertEquals(durabilidadPico, pico.durabilidad());
-
-        herrero.usar(pico);
-        assertEquals(durabilidadPico, pico.durabilidad());
-
-        // repara el pico
-        herrero.reparar(pico);
-
-        // recupera la posibilidad de usarlo 4 veces sin desgaste y luego se rompe a la quinta
-        herrero.usar(pico);
-        assertEquals(durabilidadPico, pico.durabilidad());
-
-        herrero.usar(pico);
-        assertEquals(durabilidadPico, pico.durabilidad());
-
-        herrero.usar(pico);
-        assertEquals(durabilidadPico, pico.durabilidad());
-
-        herrero.usar(pico);
-        assertEquals(durabilidadPico, pico.durabilidad());
-
-        herrero.usar(pico);
-        assertEquals(0, pico.durabilidad());
+    public void test07SeUsaHachaDeMetalConTodosLosMaterialesYSeDesgastaEnFuncionDeLaFuerzaDivididoDos() {
+        assertEquals(1, 1);
     }
+
     @Test
-    public void test09HachaFuncionaCorrectamente(){
-        Hacha hacha = new Hacha();
-        int durabilidadHacha = hacha.durabilidad();
+    public void test08SeUsaPicoDeMaderaConTodosLosMaterialesYSeDesgastaEnFuncionDeLaFuerza() {
+        assertEquals(1, 1);
+    }
 
-        hacha.usar();
-        assertEquals(durabilidadHacha -1 , hacha.durabilidad());
+    @Test
+    public void test09SeUsaPicoDePiedraConTodosLosMaterialesYSeDesgastaEnFuncionDeLaFuerzaDivididoUnoYMedio() {
+        assertEquals(1, 1);
+    }
 
-        for(int i = 0; i<4; i++ ){
-            hacha.usar();
-            }
+    @Test
+    public void test10SeUsaPicoDeMetalConTodosLosMaterialesYSeRompeAlDecimoGolpe() {
+        assertEquals(1, 1);
+    }
 
-        assertEquals(durabilidadHacha -5, hacha.durabilidad());
+    @Test
+    public void test11SeUsaPicoFinoConDiamanteYSeDesgastaDeADecimos() {
+        assertEquals(1, 1);
+    }
 
-
+    @Test
+    public void test11SeUsaPicoFinoConTodosLosMaterialesExceptoDiamanteYNoSeDesgasta() {
+        assertEquals(1, 1);
     }
 }
---
+++ test/algocraft/test/herramienta/MaterialTests.java
@@ -0,0 +1,63 @@
+package algocraft.test.herramienta;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MaterialTests {
+
+    @Test
+    public void test01SeCreaMaderaConUnaDurabilidad() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test02SeCreaPiedraConUnaDurabilidad() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test03SeCreaMetalConUnaDurabilidad() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test04SeCreaDiamanteConUnaDurabilidad() {
+        assertEquals(1, 1);
+    }
+
+    // Test contra Madera
+    @Test
+    public void test05SeUsaHachaDeMaderaEnMaterialMaderaYSeReduceDurabilidad() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test06SeUsaPicoDeMaderaEnMaterialMaderaYNoReduceDurabilidad() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test07SeUsaHachaDePiedraEnMaterialMaderaYSeReduceDurabilidad() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test08SeUsaPicoDePiedraEnMaterialMaderaYNoReduceDurabilidad() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test09SeUsaHachaDeMetalEnMaterialMaderaYSeReduceDurabilidad() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test10SeUsaPicoDeMetalEnMaterialMaderaYNoReduceDurabilidad() {
+        assertEquals(1, 1);
+    }
+    // Fin Test contra Madera
+
+    // TODO: Agregar tests idem Madera, pero con Piedra y Metal. Ojo que el enunciado nombra algunos, los otros serian supuestos nuestros
+
+}
