aa4b1ec0cb9c674be799ad678b024b4d24571325
jeremias98 <ramirezjeremias98@gmail.com>
2019-06-03 19:13:16 -0300

    Tests

--- test/fiuba/algo3/algocraft/MaterialTests.java
+++ test/fiuba/algo3/algocraft/MaterialTests.java
@@ -89,16 +89,71 @@ public class MaterialTests {
     public void test09SeUsaHachaDeMetalEnMaterialMaderaYSeReduceDurabilidad() {
         assertEquals(1, 1);
     }
-
     @Test
     public void test10SeUsaPicoDeMetalEnMaterialMaderaYNoReduceDurabilidad() {
         assertEquals(1, 1);
     }
+    @Test
+    public void test11SeUsaPicoFinoDePiedraContraMaderaNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test12SeUsaPicoFinoDeMetalContraMaderaNoReduceDurabilidad() { assertEquals(1,1); }
     // Fin Test contra Madera
 
+    // Tests contra Piedra
+    @Test
+    public void test13SeUsaHachaDeMaderaContraPiedraNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test14SeUsaHachaDePiedraContraPiedraNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test15SeUsaHachaDeMetalContraPiedraNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test16SeUsaPicoDeMaderaContraPiedraSeReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test17SeUsaPicoDePiedraContraPiedraSeReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test18SeUsaPicoDeMetalContraPiedraSeReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test19SeUsaPicoFinoDePiedraContraPiedraSeReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test20SeUsaPicoFinoDeMetalContraPiedraSeReduceDurabilidad() { assertEquals(1,1); }
+    // Fin Tests contra Piedra
 
-    /* TODO: Agregar tests idem Madera, pero con Piedra y Metal. Ojo que el enunciado nombra algunos,
-             los otros serian supuestos nuestros
-     */
+    // Tests contra Metal
+    @Test
+    public void test21SeUsaHachaDeMaderaContraMetalNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test22SeUsaHachaDePiedraContraMetalNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test23SeUsaHachaDeMetalContraMetalNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test24SeUsaPicoDeMaderaContraMetalNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test25SeUsaPicoDePiedraContraMetalSeReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test26SeUsaPicoDeMetalContraMetalSeReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test27SeUsaPicoFinoDePiedraContraMetalSeReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test28SeUsaPicoFinoDeMetalContraMetalSeReduceDurabilidad() { assertEquals(1,1); }
+    // Fin Tests contra Metal
+
+    // Tests contra Diamante
+    @Test
+    public void test29SeUsaHachaDeMaderaContraDiamanteNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test30SeUsaHachaDePiedraContraDiamanteNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test31SeUsaHachaDeMetalContraDiamanteNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test32SeUsaPicoDeMaderaContraDiamanteNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test33SeUsaPicoDePiedraContraDiamanteNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test34SeUsaPicoDeMetalContraDiamanteNoReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test35SeUsaPicoFinoDePiedraContraDiamanteSeReduceDurabilidad() { assertEquals(1,1); }
+    @Test
+    public void test36SeUsaPicoFinoDeMetalContraDiamanteSeReduceDurabilidad() { assertEquals(1,1); }
+    // Fin Tests contra Diamante
 
 }
