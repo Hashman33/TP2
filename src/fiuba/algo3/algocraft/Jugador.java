8865419667bacf8e51726d5aa225d7cd4c7fb945
jeremias98 <ramirezjeremias98@gmail.com>
2019-06-03 21:23:13 -0300

    Terminados tests de comportamiento contra todos los materiales

---
+++ src/fiuba/algo3/algocraft/Jugador.java
@@ -0,0 +1,20 @@
+package fiuba.algo3.algocraft;
+
+public class Jugador {
+
+    public Inventario inventario;
+
+    Jugador() {
+
+        Hacha hachaMadera = new Hacha(new HerramientaMadera());
+
+        this.inventario = new Inventario();
+        this.inventario.agregar(hachaMadera);
+        this.inventario.equipar(hachaMadera);
+    }
+
+    public Herramienta herramientaEquipada() {
+        return this.inventario.herramientaEquipada;
+    }
+
+}
