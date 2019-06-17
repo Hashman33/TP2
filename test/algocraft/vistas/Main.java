a65eafc808af87bed92fc4a53aedacb749e987de
jeremias98 <ramirezjeremias98@gmail.com>
2019-06-17 20:36:25 -0300

    Main en Views

--- test/algocraft/vistas/Main.java
+++ test/algocraft/vistas/Main.java
@@ -1,22 +1,46 @@
 package algocraft.vistas;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.io.InputStream;
 
 public class Main extends Application {
 
-    private static final int ANCHO = 720;
-    private static final int ALTO = 480;
+    // Poner la resolucion que quieran. Yo la setee en HD
+    public static final int ANCHO = 1280;
+    public static final int ALTO = 720;
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), ANCHO, ALTO);
+
+        stage.setTitle("AlgoCraft - Grupo 13");
+
+        InputStream i = getClass().getResourceAsStream("/recursos/fuentes/Minecrafter.Alt.ttf");
+        Font fuenteTitulo = Font.loadFont(i, 54);
+
+        Label titulo = new Label("ALGOCRAFT");
+        titulo.setFont(fuenteTitulo);
+        titulo.setTextFill(Color.web("#FFF", 0.9));
+
+        BackgroundImage fondo = new BackgroundImage(new Image("/recursos/fondos/fondo1.jpg", ANCHO, ALTO, false, true), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+
+        StackPane root = new StackPane();
+
+        // Agrego los controles
+        root.getChildren().add(titulo);
+        root.setBackground(new Background(fondo));
+
+        Scene scene = new Scene(root, ANCHO, ALTO);
         stage.setScene(scene);
         stage.show();
     }
